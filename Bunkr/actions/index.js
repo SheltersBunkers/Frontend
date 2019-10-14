29535540ae5854894e83f411969bca8d0448851d
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-10-14 17:06:28 -0500

    backbones of the actions made/ addded AsyncStorage to store token

--- Bunkr/actions/index.js
+++ Bunkr/actions/index.js
@@ -1,5 +1,6 @@
 import { axiosWithAuth } from "../utls/axiosWithAuth";
 import axios from 'axios';
+import { AsyncStorage } from 'react-native';
 
 export const FETCHING_USER_DATA = "FETCHING_USER_DATA";
 export const GET_USER_DATA_SUCCESS = "GET_USER_DATA_SUCCESS";
@@ -31,3 +32,34 @@ export const get_user = () => dispatch => {
 
         })
 }
+
+
+export const get_comments_by_id = (id) => {
+    dispatch({ type: FETCHING_COMMENTS_BY_SHELTER_ID })
+
+    axios.get('')
+        .then()
+        .catch()
+}
+
+export const get_locations = () => {
+    dispatch({ type: FETCHING_LOCATIONS })
+
+    axios.get('')
+        .then()
+        .catch()
+}
+
+export const post_comment_to_shelter = async (id, message) => {
+    dispatch({ type: POSTING })
+
+    const token = await AsyncStorage.getItem('token');
+    
+    return axiosWithAuth(token)
+        .then()
+        .catch()
+}
+
+
+// to save token 
+// await AsyncStorage.setItem('@MySuperStore:key', 'I like to save it.');
