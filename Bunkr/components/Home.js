20a1d52033b991c216a709d94c4fc48f737f8408
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-10-19 21:06:11 -0500

    found background photo for landing page

--- Bunkr/components/Home.js
+++ Bunkr/components/Home.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, ActivityIndicator, StyleSheet, ImageBackground } from 'react-native';
+import tornado from '../assets/tornado.jpg';
 
 const Home = ({ history }) => {
+    const [ toggle, setToggle ] = useState(false);
+
     return (
-        <View>
-        <Text>Bunkr</Text>
-        <Button title="Map" onPress={() => history.push("/map")}/>
-    </View>
+        <View style={{width: '100%', height: '100%'}}>
+            <ImageBackground source={tornado} style={{width: '110%', height: '110%'}}>
+                <View style={styles.top}>
+                    <Text style={styles.company}>Dorthy's</Text> 
+                    <Text style={styles.company1}>Bunkr</Text>
+                </View>
+                {/* <Button title="Map" onPress={() => history.push("/map")}/> */}
+                
+                <Text></Text>
+            </ImageBackground>
+        </View>
     )
 };
 
+const styles = StyleSheet.create({
+    company: {
+        fontSize: 80,
+    },
+    company1: {
+        fontSize: 80,
+        marginLeft: 20,
+        marginTop: -30
+    },
+    top: {
+        flex: 3,
+        marginTop: 70
+    }
+
+})
 
 export default Home;
